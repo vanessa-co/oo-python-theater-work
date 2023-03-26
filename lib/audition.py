a2c02186d4e2190106a5a38e25e2492d6ebed560
Vanessa <vanessa.co403@gmail.com>
2023-03-26 01:01:15 -0400

    updated theater lab

--- lib/audition.py
+++ lib/audition.py
@@ -1,11 +1,17 @@
 
 class Audition:
+
+    all_auditions = []
+
     def __init__(self, actor, location, role, hired=False):
         self.actor = actor
         self.location = location
         self.hired = hired
-        self._role = role
-        role.add_audition(self)
+        self.role = role
+        Audition.all_auditions.append(self)
 
     def call_back(self):
         self.hired = True
+
+    def __repr__(self):
+        return f"Audition(actors= '{self.actor}', location='{self.location}', role='{self.role.character_name}', hired={self.hired})"
--- lib/role.py
+++ lib/role.py
@@ -3,31 +3,31 @@ from .audition import Audition
 class Role:
     def __init__(self, character_name,):
         self.character_name = character_name
-        self._auditions = []
 
-    def add_audition(self, audition):
-        self._auditions.append(audition)
-
+      
+    @property
+    def auditions(self):
+        return [ audition for audition in Audition.all_auditions if audition.role == self]
 
     @property
     def actors(self):
-        return [audition.actor for audition in self._auditions]
+        return [audition.actor for audition in self.auditions]
 
     @property
     def locations(self):
-        return [audition.location for audition in self._auditions]
+        return [audition.location for audition in self.auditions]
 
     @property
     def lead(self):
-        for audition in self._auditions:
+        for audition in self.auditions:
             if audition.hired:
                 return audition
         return 'no actor has been hired for this role'
 
     @property
     def understudy(self):
-        hired_auditions = [audition for audition in self._auditions if audition.hired]
-        if len(hired_auditions) > 1:
-            return hired_auditions[1]
+        hired_actors = [audition for audition in self.auditions if audition.hired]
+        if len(hired_actors) > 1:
+            return hired_actors[-1]
         return 'no actor has been hired for understudy for this role'
     
